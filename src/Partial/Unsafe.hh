9912134a192c92b5624f1004f1f8a0692df75354
Andy Arvanitis <andyarvanitis@gmail.com>
2017-01-11 15:22:55 -0800

    C++: v1.2.0 updates

--- src/Partial/Unsafe.hh
+++ src/Partial/Unsafe.hh
@@ -26,6 +26,18 @@ namespace Partial_Unsafe {
   inline auto unsafePartial(const any& f) -> any {
     return f(undefined);
   }
+
+  // foreign import unsafePartialBecause :: forall a. String -> (Partial => a) -> a
+  //
+  inline auto unsafePartialBecause(const string& reason, const any& f) -> any {
+    try {
+      return unsafePartial(f);
+    } catch (std::runtime_error&) {
+      throw runtime_error("unsafePartial failed. The following "
+                          "assumption was incorrect: '" + reason + "'.");
+    }
+  }
+
 }
 
 #endif // PartialUnsafeFFI_HH
